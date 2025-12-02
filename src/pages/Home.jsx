a428d0f5576ba654ba1fb0afb64ee622359e172b
devmostafakamal <dev.mostafa2025@gmail.com>
2025-12-02 08:58:37 +0600

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Home/Hero";
 import LatestArtcile from "../components/Home/LatestArtcile";
 import Newsfeed from "../components/Home/Newsfeed";
 import OfferTiming from "../components/Home/OfferTiming";
-import ShopCollection from "../components/Home/ShopCollection";
+// import ShopCollection from "../components/Home/ShopCollection";
 import Newslatter from "../components/ui/Newslatter";
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <Featured />
       <CategoryGrid />
       <OfferTiming />
-      <ShopCollection />
+      {/* <ShopCollection /> */}
       <LatestArtcile />
       <Newslatter />
       <Newsfeed />
